afeff8725e3272ef46b34ef3fa9138989b1f1a5e
vole4ka <100771879+vole4ka@users.noreply.github.com>
2022-06-21 22:13:03 +0300

    Update JS HW_2.js

--- JS HW_2.js
+++ JS HW_2.js
@@ -1,22 +1,22 @@
 //JS HW_2
 function check_proba(proba){
-    if (typeof proba !== 'string' ){  //скрипт на вход должен принимать строку
+    if (typeof proba !== 'string' ){                    //скрипт на вход должен принимать строку
         console.log("is not a string")}
- else if((/^\s*$/).test(proba)){      //строка не должна быть пустой
+ else if((/^\s*$/).test(proba)){                        //строка не должна быть пустой
     console.log("Error!Empty string!")}
     else{
-if (proba.length<5){                  //должно быть мин 5 символов
+if (proba.length<5){                                    //должно быть мин 5 символов
     console.log("String too short! min 5")}
-if (proba.length>64){                 //должно быть максимум 64 символа
+if (proba.length>64){                                   //должно быть максимум 64 символа
     console.log("String too long! Max 64")}
-if (!/[a-zа-яёА-ЯЁA-Z]+/.test(proba)) //проверка на наличие букв
+if (!/[a-zа-яёА-ЯЁA-Z]+/.test(proba))                   //проверка на наличие букв
      {console.log("Error! No letters in the string")}
-if (!/^.*[A-ZА-ЯЁ]+.*$/.test(proba)){ //проверка на наличие буквы в верхнем регистре
+if (!/^.*[A-ZА-ЯЁ]+.*$/.test(proba)){                   //проверка на наличие буквы в верхнем регистре
      console.log("There is no uppercase letter")
 }
-if (!/\d/.test(proba)){               //должна быть хотя бы одна цифра
+if (!/\d/.test(proba)){                                 //должна быть хотя бы одна цифра
      console.log("Error! No number")}
-if (!/[@]/.test(proba)){              //должен быть символ @
+if (!/[@]/.test(proba)){                                //должен быть символ @
      console.log("Error! No @")}
 }}
 check_proba("j8kgb")
